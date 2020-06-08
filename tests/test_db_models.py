3a0123bc9bb0c14263b0e2c36101ad8641ea72e1
Ralph Quirequire <ralph.quirequire2012@gmail.com>
2020-06-08 14:17:59 +0800

    Parameterize fixture to run tests for  and  models

--- tests/test_db_models.py
+++ tests/test_db_models.py
@@ -4,31 +4,44 @@ from sqlalchemy.exc import IntegrityError
 from app.db.models import ScrapedItem, ScraperInfo, Source
 
 
-class TestScraperInfo:
+class TestModelBasics:
 
-    @pytest.fixture(scope='function')
-    def init_table(self, db_session, json_data):
-        """Inserts initial data to table."""
-        scrapers = json_data['scraper_info']
-        db_session.bulk_insert_mappings(ScraperInfo, scrapers)
+    @pytest.fixture(
+        scope='function',
+        params=[
+            (ScraperInfo, 'scraper_info'),
+            (Source, 'sources'),
+        ],
+        ids=['scraper', 'source']
+    )
+    def init_table(self, request, db_session, json_data):
+        """Inserts initial data to table.
+
+        Returns:
+            tuple: (db_session, model, data)
+        """
+        model, name = request.param
+        data = json_data[name]
+        db_session.bulk_insert_mappings(model, data)
         db_session.commit()
-        return db_session, scrapers
+        return db_session, model, data
 
-    def test_scraper_info_creation(self, init_table):
-        session, scrapers = init_table
-        count = session.query(ScraperInfo).count()
-        assert count == len(scrapers)
+    def test_new_row_insertion(self, init_table):
+        session, model, data = init_table
+        count = session.query(model).count()
+        assert count == len(data)
 
-    def test_avoids_duplicate_scraper_name(self, init_table):
-        session, scrapers = init_table
-        existing_scraper = scrapers[0]
+    def test_avoids_duplicate_rows(self, init_table):
+        session, model, data = init_table
+        existing_row = data[0]
         with pytest.raises(IntegrityError):
-            info = ScraperInfo(**existing_scraper)
-            session.add(info)
+            item = model(**existing_row)
+            session.add(item)
             session.commit()
 
-    def test_enforces_non_nullable_fields(self, db_session):
+    def test_enforces_non_nullable_fields(self, init_table):
+        session, model, data = init_table
         with pytest.raises(IntegrityError):
-            info = ScraperInfo()
-            db_session.add(info)
-            db_session.commit()
+            item = model()
+            session.add(item)
+            session.commit()
