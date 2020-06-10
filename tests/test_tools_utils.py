6db0331510eb10ad135a0860a7677483a3f13506
Ralph Quirequire <ralph.quirequire2012@gmail.com>
2020-06-10 13:35:36 +0800

    Put utils tests in class

--- tests/test_tools_utils.py
+++ tests/test_tools_utils.py
@@ -3,27 +3,22 @@ import pytest
 from app.tools.utils import get_domain_name
 
 
-@pytest.mark.parametrize(
-    'url,domain_name',
-    [
-        (
-            'http://www.overcomingbias.com/2020/06/no-recent-automation-revolution.html',
-            'www.overcomingbias.com'
-        ),
-        (
-            'https://blog.rust-lang.org/inside-rust/2020/06/08/new-inline-asm.html',
-            'blog.rust-lang.org'
-        ),
-        ('not-a-url', ''),
-    ]
-)
-def test_domain_name_getter(url, domain_name):
-    assert get_domain_name(url) == domain_name
+class TestGetDomainName:
 
+    @pytest.mark.parametrize(
+        'url,domain_name',
+        [
+            ('www.google.com/incomplete.html', ''),
+            ('blog.stackoverflow.com', ''),
+            ('not-a-url', ''),
+        ]
+    )
+    def test_invalid_urls_return_empty_str(self, url, domain_name):
+        assert get_domain_name(url) == domain_name
 
-def test_exceptions_cause_domain_getter_to_return_none(monkeypatch):
-    def mocked_urlparse():
-        raise ValueError
-    monkeypatch.setattr('app.tools.utils.urlparse', mocked_urlparse)
-    result = get_domain_name('https://www.google.com')
-    assert result is None
+    def test_exceptions_cause_domain_getter_to_return_none(self, monkeypatch):
+        def mocked_urlparse():
+            raise ValueError
+        monkeypatch.setattr('app.tools.utils.urlparse', mocked_urlparse)
+        result = get_domain_name('https://www.google.com')
+        assert result is None
